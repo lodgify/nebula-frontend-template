69859e509bea00d2f9af4f1caf92c7129b42c9c3
Juan Diego MaLó <hooandee@gmail.com>
2019-09-18 11:56:51 +0200

    feat: Chaging loader.

--- config/loaders/otherFiles.js
+++ config/loaders/otherFiles.js
@@ -1,7 +1,7 @@
 module.exports = () => [
   {
     test: /\.(woff|ttf|otf|eot|png|jpg|ico|gif)$/,
-    loader: 'file-loader',
+    loader: 'url-loader',
     options: {
       name: 'assets/[name].[hash].[ext]',
       publicPath: 'assets',
