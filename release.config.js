0e893ae18ea0a1d4fed0ead44199613d1a1276a2
boris-lodgify <45756168+boris-lodgify@users.noreply.github.com>
2019-09-20 14:27:44 +0200

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    path.join(__dirname, 'ci', 'semantic'),
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/github',
+  ],
+};
